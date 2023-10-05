11b70e8484ced7efe87acde620184a4119c1f754
Poulomi1234 <kar.poulomi2896@gmail.com>
2023-10-05 21:54:23 +0530

    JSX

--- App.js
+++ App.js
@@ -1,32 +1,44 @@
-//Nested Html structure
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-{
-  /* <div id="parent">
-    <div id="child">
-        <h1>I'm a h1 tag</h1>
-        <h2>I'm a h2 tag</h2>
-    </div>
-</div> */
-}
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ])
-);
+//JSX
+const jsxHeading = <h1 id="heading">Namste React from JSX</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// ); //creating an h1 tag, that's why it is coming from core react
+// //Nested Html structure
 
-// console.log(heading); //objects
+// {
+//   /* <div id="parent">
+//     <div id="child">
+//         <h1>I'm a h1 tag</h1>
+//         <h2>I'm a h2 tag</h2>
+//     </div>
+// </div> */
+// }
 
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root")); //using ReactDom because we are putting heading into div with root id
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm h1 tag"),
+//     React.createElement("h2", {}, "I'm h2 tag"),
+//   ])
+// );
 
-root.render(parent); //rendering the parent in your browser
+// // const heading = React.createElement(
+// //   "h1",
+// //   { id: "heading" },
+// //   "Hello World from React!"
+// // ); //creating an h1 tag, that's why it is coming from core react
+
+// // console.log(heading); //objects
+
+// console.log(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root")); //using ReactDom because we are putting heading into div with root id
+
+// root.render(parent); //rendering the parent in your browser
